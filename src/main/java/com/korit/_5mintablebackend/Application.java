310bcd9f580143cc178014865aa9850efa2e173d
S-JaeWon <ssm5170@gmail.com>
2026-01-07 16:39:07 +0900

    first commit

---
+++ src/main/java/com/korit/_5mintablebackend/Application.java
@@ -0,0 +1,13 @@
+package com.korit._5mintablebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
