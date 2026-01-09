ac4d90ed2fec6410d95383aaac3555454aa0a7fe
S-JaeWon <ssm5170@gmail.com>
2026-01-09 12:25:16 +0900

    Feat: test commit

---
+++ src/main/java/com/nullpoint/fifteenmintable/security/handler/GlobalExceptionHandler.java
@@ -0,0 +1,14 @@
+package com.nullpoint.fifteenmintable.security.handler;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+//    @ExceptionHandler(RuntimeException.class)
+//    public ResponseEntity<?> handleRuntimeException(RuntimeException e) {
+//        return ResponseEntity.ok(new ApiRespDto<>("failed", "문제가 발생했습니다. :" + e.getMessage(), null));
+//    }
+}
