4dcca87700e7b1136504e0263f640c36f99840c5
S-JaeWon <ssm5170@gmail.com>
2026-01-09 16:50:12 +0900

    Feat:entity, mapper

---
+++ src/main/java/com/nullpoint/fifteenmintable/dto/AddCommentReqDto.java
@@ -0,0 +1,21 @@
+package com.nullpoint.fifteenmintable.dto;
+
+import com.nullpoint.fifteenmintable.entity.Comment;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class AddCommentReqDto {
+    private Integer recipeId;
+    private String content;
+    private Integer userId;
+
+    public Comment toEntity() {
+        return Comment.builder()
+                .recipeId(recipeId)
+                .content(content)
+                .userId(userId)
+                .build();
+    }
+}
