2c919dc7ec48c58f0afc7f1c903bbd4835bb687a
hjhong80 <ghdgowns@gmail.com>
2026-01-12 11:15:13 +0900

    feat : repository work

---
+++ src/main/java/com/nullpoint/fifteenmintable/repository/RecipeRepository.java
@@ -0,0 +1,52 @@
+package com.nullpoint.fifteenmintable.repository;
+
+import com.nullpoint.fifteenmintable.entity.Board;
+import com.nullpoint.fifteenmintable.entity.Recipe;
+import com.nullpoint.fifteenmintable.mapper.RecipeMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+@RequiredArgsConstructor
+public class RecipeRepository {
+    private final RecipeMapper recipeMapper;
+
+    public int createRecipe(Board board) {
+        return recipeMapper.createRecipe(board);
+    }
+
+    public Optional<List<Recipe>> findAll() {
+        return recipeMapper.findAll();
+    }
+
+    public Optional<Recipe> findByRecipeId(Integer recipeId) {
+        return recipeMapper.findByRecipeId(recipeId);
+    }
+
+    public Optional<List<Recipe>> findByUserId(Integer userId) {
+        return recipeMapper.findByUserId(userId);
+    }
+
+    public Optional<List<Recipe>> findByMainCategoryId(Integer mainCategoryId) {
+        return recipeMapper.findByMainCategoryId(mainCategoryId);
+    }
+
+    public Optional<List<Recipe>> findBySubCategoryId(Integer subCategoryId) {
+        return recipeMapper.findBySubCategoryId(subCategoryId);
+    }
+
+    public Optional<List<Recipe>> findByKeyword(String keyword) {
+        return recipeMapper.findByKeyword(keyword);
+    }
+
+    public int updateRecipe(Recipe recipe) {
+        return recipeMapper.updateRecipe(recipe);
+    }
+
+    public int deleteRecipe(Integer recipeId) {
+        return recipeMapper.deleteRecipe(recipeId);
+    }
+}
