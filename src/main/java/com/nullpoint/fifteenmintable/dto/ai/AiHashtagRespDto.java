51fdfabb51009c6773c87ed663758283143411e8
S-JaeWon <ssm5170@gmail.com>
2026-01-18 17:04:03 +0900

    Feat: ai hashtag generate

---
+++ src/main/java/com/nullpoint/fifteenmintable/dto/ai/AiHashtagRespDto.java
@@ -0,0 +1,14 @@
+package com.nullpoint.fifteenmintable.dto.ai;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AiHashtagRespDto {
+    private List<String> hashtags;
+}
